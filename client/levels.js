82b095ef1340d0cace1db3470874f7363c73fdfe
Michael Shulman <shulman@sandiego.edu>
2026-09-09 00:58:09 -0700

    label-block stage and hints

--- client/levels.js
+++ client/levels.js
@@ -88,6 +88,7 @@ export const LEVELS = [
                     conclusion: { ty: "P∧P" },
                     trivial: true,
                     autoComplete: true,
+                    hint: "contractionHint",
                 },
                 {
                     parameters: [ { name: "P", ty: "Type" }, { name: "Q", ty: "Type" } ],
@@ -382,20 +383,47 @@ export const LEVELS = [
                     hypotheses: [ { ty: "P⇒Q" }, { ty: "R⇒S" }, ],
                     conclusion: { ty: "(P∧R)⇒(Q∧S)" },
                 },
+            ],
+          },
+          { name: "∧⇒🏷",
+            rules: [ "andE", "andI", "impE", "impI", "asc" ],
+            levels: [
+                {
+                    parameters: [ { name: "P", ty: "Type" }, { name: "Q", ty: "Type" } ],
+                    variables: [ ],
+                    hypotheses: [ { ty: "P" }, { ty: "Q" } ],
+                    conclusion: { ty: "((P∧Q)∧(P∧Q))∧(P∧Q)" },
+                    maxrules: 3,
+                },
+                {
+                    parameters: [ { name: "P", ty: "Type" }, { name: "Q", ty: "Type" }, { name: "R", ty: "Type" } ],
+                    variables: [ ],
+                    hypotheses: [ { ty: "P⇒Q" }, { ty: "Q⇒R" } ],
+                    conclusion: { ty: "(P⇒R)∧(P⇒R)" },
+                    maxrules: 4,
+                },
                 {
                     parameters: [ { name: "P", ty: "Type" }, { name: "Q", ty: "Type" }, { name: "R", ty: "Type" }, { name: "S", ty: "Type" }, { name: "T", ty: "Type" } ],
                     variables: [ ],
                     hypotheses: [ { ty: "P⇒Q" }, { ty: "Q⇒R" }, { ty: "(P⇒R)⇒S" }, { ty: "(P⇒R)⇒T" } ],
                     conclusion: { ty: "S∧T" },
-                },
-                {
-                    parameters: [ { name: "P", ty: "Type" }, { name: "Q", ty: "Type" }, { name: "R", ty: "Type" }, { name: "V", ty: "Type" }, { name: "W", ty: "Type" } ],
-                    variables: [ ],
-                    hypotheses: [ { ty: "P⇒Q" }, { ty: "Q⇒R" }, { ty: "(P⇒R)⇒V" }, { ty: "(P⇒R)⇒W" } ],
-                    conclusion: { ty: "V∧W" },
-                    extrarules: [ "asc" ],
                     maxrules: 7,
                     hint: "redexHint"
+                },
+                {
+                    parameters: [ { name: "P", ty: "Type" }, { name: "Q", ty: "Type" }, { name: "R", ty: "Type" }, { name: "S", ty: "Type" }, { name: "T", ty: "Type" } ],
+                    variables: [ ],
+                    hypotheses: [ { ty: "P⇒Q" }, { ty: "P⇒R" }, { ty: "(P⇒(Q∧R))⇒S" }, { ty: "(P⇒(Q∧R))⇒T" } ],
+                    conclusion: { ty: "S∧T" },
+                    maxrules: 8,
+                },
+                {
+                    parameters: [ { name: "P", ty: "Type" }, { name: "Q", ty: "Type" }, { name: "R", ty: "Type" }, { name: "S", ty: "Type" }, { name: "T", ty: "Type" } ],
+                    variables: [ ],
+                    hypotheses: [ { ty: "P⇒Q" }, { ty: "Q⇒R" }, { ty: "R⇒S" }, { ty: "S⇒T" } ],
+                    conclusion: { ty: "(P⇒S)∧(P⇒T)" },
+                    maxrules: 9,
+                    hint: "redex2Hint"
                 },
             ]
           },
