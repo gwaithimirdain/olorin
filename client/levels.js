8fd762376b29b3ac2d2818f794009f4a70c1a6f4
Michael Shulman <shulman@sandiego.edu>
2026-09-19 13:35:01 -0700

    6-5-4 parameters should be variables

--- client/levels.js
+++ client/levels.js
@@ -1712,8 +1712,8 @@ export const LEVELS = [
                     },
                 },
                 {
-                    parameters: [ { name: "x", ty: "𝕊" }, { name: "q", ty: "ℝ" } ],
-                    variables: [ ],
+                    parameters: [ ],
+                    variables: [ { name: "x", ty: "𝕊" }, { name: "q", ty: "ℝ" } ],
                     hypotheses: [ { ty: "x is positive infinite" }, { ty: "q>0" } ],
                     conclusion: { ty: "x·q is positive infinite" },
                 },
